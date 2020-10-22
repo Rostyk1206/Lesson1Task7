7346e328ba8ca32225ca1a72f8874de56308c18b
unknown <rostykvoznyak@gmail.com>
2020-10-22 23:13:37 +0300

    Lesso1Task7

---
+++ Lesson1Task7/Lesson1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson1Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'M';
+            Console.WriteLine("Gender:{0}",a);
+        }
+    }
+}
